bb438399568f6b0b6d3c4b9f018a8e3404b15b07
Анна Белоусова <yanuta2000@mail.ru>
2019-05-16 19:31:51 +0600

    first commit

---
+++ src/lab/pkg18/Lab18.java
@@ -0,0 +1,12 @@
+package lab.pkg18;
+
+/**
+ *
+ * @author yanut_000
+ */
+public class Lab18 {
+
+    public static void main(String[] args) {
+        NootepadWindow notepad = new NootepadWindow("Блокнот", 600, 700);
+    }
+}
